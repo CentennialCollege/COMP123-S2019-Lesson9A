d9921f21369b29ef3c70c6acb9c8146c67fa1bbb
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-07-10 18:46:34 -0400

    finalized the Numeric Keyboard Logic

--- COMP123-S2019-Lesson9A/CalculatorForm.Designer.cs
+++ COMP123-S2019-Lesson9A/CalculatorForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace COMP123_S2019_Lesson9A
 {
-    partial class Lesson9Form
+    partial class CalculatorForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,7 +30,7 @@
         {
             this.SevenButton = new System.Windows.Forms.Button();
             this.NumberButtonTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.ZeroButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
             this.ThreeButton = new System.Windows.Forms.Button();
             this.TwoButton = new System.Windows.Forms.Button();
             this.OneButton = new System.Windows.Forms.Button();
@@ -39,11 +39,12 @@
             this.FourButton = new System.Windows.Forms.Button();
             this.NineButton = new System.Windows.Forms.Button();
             this.EightButton = new System.Windows.Forms.Button();
-            this.DoneButton = new System.Windows.Forms.Button();
-            this.ResultLabel = new System.Windows.Forms.Label();
             this.DecimalButton = new System.Windows.Forms.Button();
             this.BackButton = new System.Windows.Forms.Button();
             this.ClearButton = new System.Windows.Forms.Button();
+            this.ZeroButton = new System.Windows.Forms.Button();
+            this.DoneButton = new System.Windows.Forms.Button();
+            this.HeightLabel = new System.Windows.Forms.Label();
             this.NumberButtonTableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,6 +59,7 @@
             this.SevenButton.Name = "SevenButton";
             this.SevenButton.Size = new System.Drawing.Size(64, 41);
             this.SevenButton.TabIndex = 0;
+            this.SevenButton.Tag = "7";
             this.SevenButton.Text = "7";
             this.SevenButton.UseVisualStyleBackColor = false;
             this.SevenButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
@@ -72,7 +74,6 @@
             this.NumberButtonTableLayoutPanel.Controls.Add(this.ResultLabel, 0, 0);
             this.NumberButtonTableLayoutPanel.Controls.Add(this.ThreeButton, 2, 3);
             this.NumberButtonTableLayoutPanel.Controls.Add(this.TwoButton, 1, 3);
-            this.NumberButtonTableLayoutPanel.Controls.Add(this.OneButton, 0, 3);
             this.NumberButtonTableLayoutPanel.Controls.Add(this.SixButton, 2, 2);
             this.NumberButtonTableLayoutPanel.Controls.Add(this.FiveButton, 1, 2);
             this.NumberButtonTableLayoutPanel.Controls.Add(this.FourButton, 0, 2);
@@ -84,6 +85,7 @@
             this.NumberButtonTableLayoutPanel.Controls.Add(this.ClearButton, 3, 2);
             this.NumberButtonTableLayoutPanel.Controls.Add(this.ZeroButton, 0, 4);
             this.NumberButtonTableLayoutPanel.Controls.Add(this.DoneButton, 3, 3);
+            this.NumberButtonTableLayoutPanel.Controls.Add(this.OneButton, 0, 3);
             this.NumberButtonTableLayoutPanel.Location = new System.Drawing.Point(12, 191);
             this.NumberButtonTableLayoutPanel.Name = "NumberButtonTableLayoutPanel";
             this.NumberButtonTableLayoutPanel.RowCount = 5;
@@ -95,21 +97,22 @@
             this.NumberButtonTableLayoutPanel.Size = new System.Drawing.Size(280, 238);
             this.NumberButtonTableLayoutPanel.TabIndex = 1;
             // 
-            // ZeroButton
+            // ResultLabel
             // 
-            this.ZeroButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.ResultLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ZeroButton.BackColor = System.Drawing.Color.SteelBlue;
-            this.NumberButtonTableLayoutPanel.SetColumnSpan(this.ZeroButton, 2);
-            this.ZeroButton.ForeColor = System.Drawing.Color.White;
-            this.ZeroButton.Location = new System.Drawing.Point(3, 191);
-            this.ZeroButton.Name = "ZeroButton";
-            this.ZeroButton.Size = new System.Drawing.Size(134, 44);
-            this.ZeroButton.TabIndex = 10;
-            this.ZeroButton.Text = "0";
-            this.ZeroButton.UseVisualStyleBackColor = false;
-            this.ZeroButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
+            this.ResultLabel.BackColor = System.Drawing.Color.White;
+            this.ResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NumberButtonTableLayoutPanel.SetColumnSpan(this.ResultLabel, 4);
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLabel.Location = new System.Drawing.Point(3, 0);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(274, 47);
+            this.ResultLabel.TabIndex = 2;
+            this.ResultLabel.Tag = "result";
+            this.ResultLabel.Text = "0";
+            this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // ThreeButton
             // 
@@ -122,6 +125,7 @@
             this.ThreeButton.Name = "ThreeButton";
             this.ThreeButton.Size = new System.Drawing.Size(64, 41);
             this.ThreeButton.TabIndex = 8;
+            this.ThreeButton.Tag = "3";
             this.ThreeButton.Text = "3";
             this.ThreeButton.UseVisualStyleBackColor = false;
             this.ThreeButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
@@ -137,6 +141,7 @@
             this.TwoButton.Name = "TwoButton";
             this.TwoButton.Size = new System.Drawing.Size(64, 41);
             this.TwoButton.TabIndex = 7;
+            this.TwoButton.Tag = "2";
             this.TwoButton.Text = "2";
             this.TwoButton.UseVisualStyleBackColor = false;
             this.TwoButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
@@ -152,6 +157,7 @@
             this.OneButton.Name = "OneButton";
             this.OneButton.Size = new System.Drawing.Size(64, 41);
             this.OneButton.TabIndex = 6;
+            this.OneButton.Tag = "1";
             this.OneButton.Text = "1";
             this.OneButton.UseVisualStyleBackColor = false;
             this.OneButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
@@ -167,6 +173,7 @@
             this.SixButton.Name = "SixButton";
             this.SixButton.Size = new System.Drawing.Size(64, 41);
             this.SixButton.TabIndex = 5;
+            this.SixButton.Tag = "6";
             this.SixButton.Text = "6";
             this.SixButton.UseVisualStyleBackColor = false;
             this.SixButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
@@ -182,6 +189,7 @@
             this.FiveButton.Name = "FiveButton";
             this.FiveButton.Size = new System.Drawing.Size(64, 41);
             this.FiveButton.TabIndex = 4;
+            this.FiveButton.Tag = "5";
             this.FiveButton.Text = "5";
             this.FiveButton.UseVisualStyleBackColor = false;
             this.FiveButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
@@ -197,6 +205,7 @@
             this.FourButton.Name = "FourButton";
             this.FourButton.Size = new System.Drawing.Size(64, 41);
             this.FourButton.TabIndex = 3;
+            this.FourButton.Tag = "4";
             this.FourButton.Text = "4";
             this.FourButton.UseVisualStyleBackColor = false;
             this.FourButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
@@ -212,6 +221,7 @@
             this.NineButton.Name = "NineButton";
             this.NineButton.Size = new System.Drawing.Size(64, 41);
             this.NineButton.TabIndex = 2;
+            this.NineButton.Tag = "9";
             this.NineButton.Text = "9";
             this.NineButton.UseVisualStyleBackColor = false;
             this.NineButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
@@ -227,42 +237,10 @@
             this.EightButton.Name = "EightButton";
             this.EightButton.Size = new System.Drawing.Size(64, 41);
             this.EightButton.TabIndex = 1;
+            this.EightButton.Tag = "8";
             this.EightButton.Text = "8";
             this.EightButton.UseVisualStyleBackColor = false;
             this.EightButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
-            // 
-            // DoneButton
-            // 
-            this.DoneButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DoneButton.BackColor = System.Drawing.Color.LimeGreen;
-            this.DoneButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DoneButton.ForeColor = System.Drawing.Color.White;
-            this.DoneButton.Location = new System.Drawing.Point(213, 144);
-            this.DoneButton.Name = "DoneButton";
-            this.NumberButtonTableLayoutPanel.SetRowSpan(this.DoneButton, 2);
-            this.DoneButton.Size = new System.Drawing.Size(64, 91);
-            this.DoneButton.TabIndex = 11;
-            this.DoneButton.Text = "Done";
-            this.DoneButton.UseVisualStyleBackColor = false;
-            this.DoneButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
-            // 
-            // ResultLabel
-            // 
-            this.ResultLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.ResultLabel.BackColor = System.Drawing.Color.White;
-            this.ResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.NumberButtonTableLayoutPanel.SetColumnSpan(this.ResultLabel, 4);
-            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ResultLabel.Location = new System.Drawing.Point(3, 0);
-            this.ResultLabel.Name = "ResultLabel";
-            this.ResultLabel.Size = new System.Drawing.Size(274, 47);
-            this.ResultLabel.TabIndex = 2;
-            this.ResultLabel.Text = "0";
-            this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // DecimalButton
             // 
@@ -275,8 +253,10 @@
             this.DecimalButton.Name = "DecimalButton";
             this.DecimalButton.Size = new System.Drawing.Size(64, 44);
             this.DecimalButton.TabIndex = 12;
+            this.DecimalButton.Tag = "decimal";
             this.DecimalButton.Text = ".";
             this.DecimalButton.UseVisualStyleBackColor = false;
+            this.DecimalButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
             // 
             // BackButton
             // 
@@ -291,6 +271,7 @@
             this.BackButton.Name = "BackButton";
             this.BackButton.Size = new System.Drawing.Size(64, 41);
             this.BackButton.TabIndex = 2;
+            this.BackButton.Tag = "back";
             this.BackButton.UseVisualStyleBackColor = false;
             this.BackButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
             // 
@@ -306,21 +287,78 @@
             this.ClearButton.Name = "ClearButton";
             this.ClearButton.Size = new System.Drawing.Size(64, 41);
             this.ClearButton.TabIndex = 5;
+            this.ClearButton.Tag = "clear";
             this.ClearButton.Text = "Clear";
             this.ClearButton.UseVisualStyleBackColor = false;
             this.ClearButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
             // 
-            // Lesson9Form
+            // ZeroButton
+            // 
+            this.ZeroButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ZeroButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.NumberButtonTableLayoutPanel.SetColumnSpan(this.ZeroButton, 2);
+            this.ZeroButton.ForeColor = System.Drawing.Color.White;
+            this.ZeroButton.Location = new System.Drawing.Point(3, 191);
+            this.ZeroButton.Name = "ZeroButton";
+            this.ZeroButton.Size = new System.Drawing.Size(134, 44);
+            this.ZeroButton.TabIndex = 10;
+            this.ZeroButton.Tag = "0";
+            this.ZeroButton.Text = "0";
+            this.ZeroButton.UseVisualStyleBackColor = false;
+            this.ZeroButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
+            // 
+            // DoneButton
+            // 
+            this.DoneButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DoneButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.DoneButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DoneButton.ForeColor = System.Drawing.Color.White;
+            this.DoneButton.Location = new System.Drawing.Point(213, 144);
+            this.DoneButton.Name = "DoneButton";
+            this.NumberButtonTableLayoutPanel.SetRowSpan(this.DoneButton, 2);
+            this.DoneButton.Size = new System.Drawing.Size(64, 91);
+            this.DoneButton.TabIndex = 11;
+            this.DoneButton.Tag = "done";
+            this.DoneButton.Text = "Done";
+            this.DoneButton.UseVisualStyleBackColor = false;
+            this.DoneButton.Click += new System.EventHandler(this.CalculatorButtons_Click);
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HeightLabel.BackColor = System.Drawing.Color.White;
+            this.HeightLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HeightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeightLabel.Location = new System.Drawing.Point(15, 36);
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(147, 47);
+            this.HeightLabel.TabIndex = 2;
+            this.HeightLabel.Tag = "height";
+            this.HeightLabel.Text = "0";
+            this.HeightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HeightLabel.Click += new System.EventHandler(this.HeightLabel_Click);
+            // 
+            // CalculatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(304, 441);
+            this.Controls.Add(this.HeightLabel);
             this.Controls.Add(this.NumberButtonTableLayoutPanel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.Name = "Lesson9Form";
+            this.MaximizeBox = false;
+            this.Name = "CalculatorForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.CalculatorForm_Load);
             this.NumberButtonTableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -344,6 +382,7 @@
         private System.Windows.Forms.Button DecimalButton;
         private System.Windows.Forms.Button BackButton;
         private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label HeightLabel;
     }
 }
 
---
+++ COMP123-S2019-Lesson9A/CalculatorForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lesson9A
+{
+    public partial class CalculatorForm : Form
+    {
+        // CLASS PROPERTIES
+        public string outputString { get; set; }
+        public float outputValue { get; set; }
+        public bool decimalExists { get; set; }
+
+        /// <summary>
+        /// This is the Constructor Method
+        /// </summary>
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is the shared Event Handler for all of the Calculator Buttons' Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorButtons_Click(object sender, EventArgs e)
+        {
+            Button TheButton = sender as Button;
+            var tag = TheButton.Tag.ToString();
+            int numericValue = 0;
+
+            bool numericResult = int.TryParse(tag, out numericValue);
+
+            if(numericResult)
+            {
+                int maxSize = (decimalExists) ? 5 : 3;
+
+                if(outputString == "0")
+                {
+                    outputString = tag;
+                }
+                else
+                {
+                    if(outputString.Length < maxSize)
+                    {
+                        outputString += tag;
+                    }
+                }
+                    
+                ResultLabel.Text = outputString;
+            }
+            else
+            {
+                switch (tag)
+                {
+                    case "back":
+                        removeLastCharacterFromResultLabel();
+                        break;
+                    case "done":
+                        finalizeOuput();
+                        break;
+                    case "clear":
+                        clearNumericKeyboard();
+                        break;
+                    case "decimal":
+                        addDecimalToResultLabel();
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method adds a decimal point to the resultLabel
+        /// </summary>
+        private void addDecimalToResultLabel()
+        {
+            if (!decimalExists)
+            {
+                outputString += ".";
+                decimalExists = true;
+            }
+        }
+
+
+        /// <summary>
+        /// This method finalizes and converts the outputString to a floating point value
+        /// </summary>
+        private void finalizeOuput()
+        {
+            outputValue = float.Parse(outputString);
+
+            outputValue = (float)(Math.Round(outputValue, 1));
+
+            if (outputValue < 0.1f)
+            {
+                outputValue = 0.1f;
+            }
+            HeightLabel.Text = outputValue.ToString();
+            clearNumericKeyboard();
+            NumberButtonTableLayoutPanel.Visible = false;
+        }
+
+        /// <summary>
+        /// This method removes the last character from the Result Label
+        /// </summary>
+        private void removeLastCharacterFromResultLabel()
+        {
+            var lastChar = outputString.Substring(outputString.Length - 1);
+            if (lastChar == ".")
+            {
+                decimalExists = false;
+            }
+            outputString = outputString.Remove(outputString.Length - 1);
+
+            if (outputString.Length == 0)
+            {
+                outputString = "0";
+            }
+
+            ResultLabel.Text = outputString;
+        }
+
+        /// <summary>
+        /// This method resets the numeric keyboard and related variables
+        /// </summary>
+        private void clearNumericKeyboard()
+        {
+            ResultLabel.Text = "0";
+            outputString = "0";
+            outputValue = 0.0f;
+            decimalExists = false;
+        }
+
+        /// <summary>
+        /// This is the event handler for the form load event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorForm_Load(object sender, EventArgs e)
+        {
+            clearNumericKeyboard();
+            NumberButtonTableLayoutPanel.Visible = false;
+        }
+
+        /// <summary>
+        /// This is the event handler for the HeightLabel click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HeightLabel_Click(object sender, EventArgs e)
+        {
+            NumberButtonTableLayoutPanel.Visible = true;
+        }
+    }
+}
